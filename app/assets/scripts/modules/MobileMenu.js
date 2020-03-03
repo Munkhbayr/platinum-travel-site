7ac4cc3ab62a1badacae07ef1b8f8679f01ea7ad
Munkhbayr <munkhbayrduuren@gmail.com>
2020-03-03 05:52:30 -0800

    Desktop bolon mobile header style buren huuj duuslaa

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobailMenu {
+  constructor() {
+    this.menuicon = document.querySelector(".site-header__menu-icon");
+    this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
+    this.events();
+  }
+
+  events() {
+    this.menuicon.addEventListener("click", () => this.toggleTheMenu());
+  }
+
+  toggleTheMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--is-visible");
+    this.siteHeader.classList.toggle("site-header--is-expanded");
+    this.menuicon.classList.toggle("site-header__menu-icon--close-x");
+  }
+}
+
+export default MobailMenu;
